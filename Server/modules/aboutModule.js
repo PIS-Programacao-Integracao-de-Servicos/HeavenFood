a08eb19a0d4c6a5ad65c307fad6f19f64f31a240
Jonathan Osório <jonathan.d.osorio@cgi.com>
2025-01-06 11:32:15 +0000

    Module Ajustado

--- Server/modules/aboutModule.js
+++ Server/modules/aboutModule.js
@@ -2,10 +2,10 @@
 const getAboutUs = (req, res) => {
     const aboutInfo = {
       title: "About Us",
-      description: "Welcome to Food Heaven! We are dedicated to providing the best recipes and culinary experiences. Our team works tirelessly to bring you a variety of delicious and healthy recipes from around the world.",
-      mission: "To inspire cooking at home by making recipes easy, accessible, and fun for everyone.",
-      vision: "To be the world's most trusted recipe platform.",
-      values: ["Innovation", "Integrity", "Sustainability", "Community"],
+      description: "Bem-vindo ao Food Heaven! Nós nos dedicamos a fornecer as melhores receitas e experiências culinárias. Nossa equipe trabalha incansavelmente para trazer a você uma variedade de receitas deliciosas e saudáveis ​​do mundo todo.",
+      mission: "Para inspirar a culinária em casa, tornando receitas fáceis, acessíveis e divertidas para todos.",
+      vision: "Ser a plataforma de receitas mais confiável do mundo.",
+      values: ["Inovação", "Integridade", "Sustentabilidade", "Comunidade"],
     };
   
     res.status(200).json(aboutInfo);
