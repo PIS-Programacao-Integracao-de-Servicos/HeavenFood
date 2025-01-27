43084397d5632f985ff1df6ffa13aa38458eceaf
jonathan-osorio <jonathan.d.osorio@cgi.com>
2025-01-27 12:23:36 +0000

    Fix bug - DEPENDECIA BD E API

--- Server/models/recipeModel.js
+++ Server/models/recipeModel.js
@@ -1,18 +1,23 @@
 const db = require('../config/dbConnection');
 
 const getAllRecipes = async () => {
-    const query = `
-        SELECT 
-            Receita.id,
-            Receita.image_url,
-            Receita.nome,
-            Receita.descricao_preparacao, 
-            Categorias.nome AS categoria 
-        FROM Receita 
-        LEFT JOIN Categorias ON Receita.categoria_id = Categorias.id
-    `;
-    const [results] = await db.promise().query(query);
-    return results;
+    let results = [];
+    try {
+        const query = `
+            SELECT 
+                Receita.id,
+                Receita.image_url,
+                Receita.nome,
+                Categorias.nome AS categoria 
+            FROM Receita    
+            LEFT JOIN Categorias ON Receita.categoria_id = Categorias.id
+        `;
+
+        [results] = await db.promise().query(query);
+        return results;
+    } catch (error) {
+        console.error('Erro ao acessar o banco de dados:', error.message);
+    }
 };
 
 module.exports = {
--- Server/services/recipeService.js
+++ Server/services/recipeService.js
@@ -3,40 +3,53 @@ const https = require('https');
 const recipeModel = require('../models/recipeModel');
 
 const getRecipes = async () => {
-    const dbResults = await recipeModel.getAllRecipes();
+    let dbResults = await recipeModel.getAllRecipes();
+    dbResults = Array.isArray(dbResults) ? dbResults : [];
+    let apiRecipes = [];
+    try {
+        const agent = new https.Agent({
+            rejectUnauthorized: false,
+        });
 
-    const agent = new https.Agent({
-        rejectUnauthorized: false,
-    });
+        const apiResponse = await axios.get('https://www.themealdb.com/api/json/v1/1/search.php?s=', {
+            httpsAgent: agent,
+        });
+        apiRecipes = apiResponse.data.meals || [];
 
-    const apiResponse = await axios.get('https://www.themealdb.com/api/json/v1/1/search.php?s=', {
-        httpsAgent: agent,
-    });
-    const apiRecipes = apiResponse.data.meals || [];
+    } catch (error) {
+        console.error('Erro ao buscar todas as receitas na API:', error.message);
+    }
 
     const formattedApiRecipes = apiRecipes.map((meal) => ({
-        id: meal.idMeal,
+        id: parseInt(meal.idMeal),
         nome: meal.strMeal,
-        descricao_preparacao: meal.strInstructions,
         categoria: meal.strCategory,
         image_url: meal.strMealThumb,
+        source: 'api'
     }));
 
     return [...dbResults, ...formattedApiRecipes].slice(0, 4);
 };
 
 const getAllRecipes = async () => {
-    const dbResults = await recipeModel.getAllRecipes();
+    let dbResults = await recipeModel.getAllRecipes();
+    dbResults = Array.isArray(dbResults) ? dbResults : [];
+    let apiRecipes = [];
+    try {
+        const agent = new https.Agent({
+            rejectUnauthorized: false,
+        });
 
-    const agent = new https.Agent({
-        rejectUnauthorized: false,
-    });
+        const apiResponse = await axios.get('https://www.themealdb.com/api/json/v1/1/search.php?s=', {
+            httpsAgent: agent,
+        });
+        apiRecipes = apiResponse.data.meals || [];
 
-    const apiResponse = await axios.get('https://www.themealdb.com/api/json/v1/1/search.php?s=', {
-        httpsAgent: agent,
-    });
-    const apiRecipes = apiResponse.data.meals || [];
+    } catch (error) {
+        console.error('Erro ao buscar todas as receitas na API:', error.message);
+    }
 
+    
     const formattedApiRecipes = apiRecipes.map((meal) => ({
         id: parseInt(meal.idMeal),
         nome: meal.strMeal,
